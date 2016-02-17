78151ea2cddd15d8e42d9775d343b4148df4bbca
Pascal Raszyk <pr@mediatrooper.de>
2016-02-17 18:45:08 +0100

    updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 ;(function(){
 
 	$(function(){
-		//JustSticky('.navigation-bar');
+		var $window = $(window);
+		var $htmlbody = $("body");
+		$window.on("scroll", function(){
+			if ($htmlbody.scrollTop() > 30){
+				$(".navigation-bar").addClass("-solid");
+			} else {
+				$(".navigation-bar").removeClass("-solid");
+			}
+		});
 	});
 
 })();
--- js/scripts.js
+++ js/scripts.js
@@ -45,27 +45,11 @@ $(document).ready(function() {
 
 	/***************** Fancybox ******************/
 
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
+	
 
 });
 
-$(document).ready(function() {
-	$("a.single_image").fancybox({
-		padding: 4,
-	});
-});
+
 
 /***************** Nav Transformicon ******************/
 
@@ -73,18 +57,21 @@ $(document).ready(function() {
 $(".nav-toggle").click(function() {
 	$(this).toggleClass("active");
 	$(".overlay-boxify").toggleClass("open");
+	$("body").toggleClass("open-menu");
 });
 
 /* When user clicks a link */
 $(".overlay ul li a").click(function() {
 	$(".nav-toggle").toggleClass("active");
 	$(".overlay-boxify").toggleClass("open");
+	$("body").toggleClass("open-menu");
 });
 
 /* When user clicks outside */
 $(".overlay").click(function() {
 	$(".nav-toggle").toggleClass("active");
 	$(".overlay-boxify").toggleClass("open");
+	$("body").toggleClass("open-menu");
 });
 
 /***************** Smooth Scrolling ******************/
